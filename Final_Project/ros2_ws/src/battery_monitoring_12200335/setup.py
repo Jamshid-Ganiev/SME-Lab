ba4ef708d73d8e00dd6f9eed6119e713adcffd75
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-12-02 17:44:18 -0800

    Update launch files

--- Final_Project/ros2_ws/src/battery_monitoring_12200335/setup.py
+++ Final_Project/ros2_ws/src/battery_monitoring_12200335/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'battery_monitoring_12200335'
@@ -10,18 +12,20 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/battery_monitor_launch.py']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Jamshid Ganiev',
     maintainer_email='12200335@inha.edu',
     description='Battery monitoring script, subscribing to a custom battery state topic, and displaying percentage and charging status.',
-    license='MIT',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'battery_checker = battery_monitoring_12200335.battery_state_checker:main'
         ],
+        'launch.frontend.launch_extension': ['launch = launch.frontend.launch:LaunchExtension'],
     },
 )
